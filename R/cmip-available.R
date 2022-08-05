ecdae7addb135807b80ebbaaceb548afdf923062
Elio Campitelli <eliocampitelli@gmail.com>
2022-08-05 17:25:22 -0300

    Refactors cmip_available() to use cmip_parse_search() Now it actually Closes #5

--- R/cmip-available.R
+++ R/cmip-available.R
@@ -12,27 +12,14 @@
 cmip_available <- function(root = cmip_root_get()) {
   infos <- list.files(root, pattern = "model.info", recursive = TRUE, full.names = TRUE)
 
-  cmip6_folder_template <- gsub("%\\(", "", cmip6_folder_template)
-  cmip6_folder_template <- gsub("\\)s", "", cmip6_folder_template)
+  data <- lapply(infos, jsonlite::read_json, simplifyVector = TRUE)
 
-  vars <- c(strsplit(cmip6_folder_template, "/")[[1]],
-            "variable_long_name",
-            "datetime_start",
-            "datetime_stop",
-            "nominal_resolution")
-  vars <- setdiff(vars, "root")
+  files <- lapply(infos, function(info) {
+    list.files(dirname(info), pattern = ".nc", recursive = TRUE, full.names = TRUE)
+  })
 
-  data <- Reduce(rbind,
-         lapply(infos, function(info) {
-           files <- list.files(dirname(info), pattern = ".nc", recursive = TRUE, full.names = TRUE)
-           data <- jsonlite::read_json(info, simplifyVector = TRUE)
-
-           info <- as.data.frame(data[vars])
-
-           info$files <- list(files)
-           info$full_info <- list(data)
-           info
-         }))
+  data <- cmip_parse_search(data)
+  data$files <- files
 
   return(data)
 }
