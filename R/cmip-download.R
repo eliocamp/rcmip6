dc623141295675d538a78c6518d5147ffb6bf0ec
Elio Campitelli <eliocampitelli@gmail.com>
2022-08-10 20:16:26 -0300

    Adds error if download fails

--- R/cmip-download.R
+++ R/cmip-download.R
@@ -69,6 +69,7 @@ cmip_download_one <- function(result,
                             times = 10,
                             httr::write_disk(file, overwrite = TRUE),
                             httr::progress())
+    httr::stop_for_status(response, task = NULL)
 
     log <- paste(as.character(as.POSIXlt(Sys.time(), tz = "UTC")), "-", user)
     writeLines(c(log, comment), file.path(result_dir(i), paste0(tools::file_path_sans_ext(i$title), ".log")))
