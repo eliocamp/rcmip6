80f5a80e3417d0c0c558d851298e87133a64cbc3
Elio Campitelli <eliocampitelli@gmail.com>
2024-02-07 16:30:17 -0300

    Makes better available

--- R/cmip-available.R
+++ R/cmip-available.R
@@ -5,11 +5,17 @@
 #'
 #' @inheritParams cmip_download
 #'
+#' @details
+#' `cmip_available_legacy()` reads from the older way of storing information
+#' and will only work for files downloaded with version <= 0.0.2 of the package.
+#' `cmip_available_convert()` will try to read your downloads and convert it
+#' to the new store.
+#'
+#'
 #' @returns
 #' A data.frame.
 #'
 #' @export
-
 cmip_available <- function(root = cmip_root_get()) {
   files <- list.files(root, pattern = ".json", full.names = TRUE)
   info <- lapply(files, function(f) {
@@ -29,6 +35,9 @@ cmip_available <- function(root = cmip_root_get()) {
   data.table::rbindlist(info)
 }
 
+
+#' @rdname cmip_available
+#' @export
 cmip_available_legacy <- function(root = cmip_root_get()) {
   infos <- list.files(root, pattern = "model.info",
                       recursive = TRUE, full.names = TRUE)
@@ -46,5 +55,29 @@ cmip_available_legacy <- function(root = cmip_root_get()) {
 
   data$files <- files
 
+
+
   return(data)
 }
+
+#' @rdname cmip_available
+#' @export
+cmip_available_convert <- function(root = cmip_root_get()) {
+  available <- cmip_available_legacy(root)
+
+  info <- available$info
+
+  ## Info has almost the same information than results and it's used for storage
+  ## Results do have some other columns, add them just in case.
+  for (i in seq_along(info)) {
+    missing_columns <- colnames(available)[!(colnames(available) %in% colnames(info[[i]]))]
+    info[[i]][missing_columns] <- lapply(available[i, missing_columns, with = FALSE],
+                                         rep, times = nrow(info[[i]]))
+  }
+
+  info <- flatten_info(info)
+
+  file <- file.path(root, "legacy.json")
+  jsonlite::write_json(info, file)
+}
+
